b44d44111991c11cdcab3d9ad81c7047fa7874be
FYshoresy <andrey.abramtsov69@gmail.com>
2024-12-11 16:30:49 +0700

    mail.py

--- mail.py
+++ mail.py
@@ -2,8 +2,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     if "@" not in sender or not (sender.endswith(".com") or sender.endswith(".ru") or sender.endswith(".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
         return
-    if "@" not in recipient or not (
-            recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
+    if "@" not in recipient or not (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
         return
     if sender == recipient:
